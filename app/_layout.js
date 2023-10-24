6104f45cb1a9bff357f2923da45d9eaf67a20789
BJ Snowden <bsnowden@BJs-MacBook-Pro.local>
2023-10-24 10:52:51 -0400

    Upload sample expo app

---
+++ app/_layout.js
@@ -0,0 +1,6 @@
+import React, { useContext, useState } from 'react';
+import { Stack, Link } from 'expo-router';
+
+export default function HomeLayout() {
+    return <Stack screenOptions={{ headerShown: false }}/>;
+};
